55df8aac4c5a8ee985db747e3f3a7768d6cd1207
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-09-06 23:13:36 +0300

    Добавил таблицу UITableView

--- FakeNFT/Scenes /Statistics/StatisticViewController.swift
+++ FakeNFT/Scenes /Statistics/StatisticViewController.swift
@@ -1,7 +1,99 @@
 import UIKit
 
-final class StatisticViewController: UIViewController {
+final class StatisticsViewController: UIViewController {
+    
+    private lazy var sortButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let image = Images.Common.sortBtn?.withTintColor(UIColor.segmentActive, renderingMode: .alwaysOriginal)
+        button.setImage(image ?? UIImage(), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(nil, action: #selector(showSortAlert), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var scoreTable: UITableView = {
+        let table = UITableView()
+        table.separatorStyle = .none
+        table.rowHeight = 88
+        table.showsVerticalScrollIndicator = false
+        table.showsHorizontalScrollIndicator = false
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .systemBackground
+        super.viewDidLoad()
+        
+        setupTable()
+        constraintView()
     }
+    
+    private func setupTable() {
+        scoreTable.delegate = self
+        scoreTable.dataSource = self
+        scoreTable.register(StatisticsUserTableViewCell.self, 
+                            forCellReuseIdentifier: StatisticsUserTableViewCell.reuseIdentifier)
+    }
+    
+    private func constraintView() {
+        view.addSubview(sortButton)
+        view.addSubview(scoreTable)
+        
+        NSLayoutConstraint.activate([
+            
+            sortButton.widthAnchor.constraint(equalToConstant: 42),
+            sortButton.heightAnchor.constraint(equalTo: sortButton.widthAnchor),
+            sortButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            sortButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
+            
+            scoreTable.topAnchor.constraint(equalTo: sortButton.bottomAnchor, constant: 20),
+            scoreTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            scoreTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            scoreTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -13)
+        ])
+    }
+    
+    @objc private func showSortAlert() {
+        let alert = UIAlertController(title: Strings.Alerts.sortTitle, message: nil, preferredStyle: .actionSheet)
+        
+        let name = UIAlertAction(title: Strings.Alerts.sortByName, style: .default) { _ in
+            print("По имени")
+        }
+        let rating = UIAlertAction(title: Strings.Alerts.sortByRating, style: .default) { _ in
+            print("По рейтингу")
+        }
+        let close = UIAlertAction(title: Strings.Alerts.closeBtn, style: .cancel)
+        
+        alert.addAction(name)
+        alert.addAction(rating)
+        alert.addAction(close)
+        
+        present(alert, animated: true)
+        
+    }
+}
+
+extension StatisticsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StatisticsUserTableViewCell.reuseIdentifier, for: indexPath) as? StatisticsUserTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let model = StatisticsUserCellModel(
+            cellIndex: indexPath.row + 1,
+            avatarImage: UIImage(named: "userpick")!,
+            name: "Nagibator",
+            score: 321
+        )
+        cell.configure(with: model)
+        return cell
+    }
+}
+
+extension StatisticsViewController: UITableViewDelegate {
+    
 }
