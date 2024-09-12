e6efaa85296af5e10c6674bf5ab65fba27507b88
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-09-13 01:48:43 +0300

    Сделал сохранение порядка сортировки и восстановление сортировки при перезапуске

--- FakeNFT/Scenes /Statistics/Rating/StatisticViewModel.swift
+++ FakeNFT/Scenes /Statistics/Rating/StatisticViewModel.swift
@@ -25,26 +25,53 @@ final class StatisticViewModel: StatisticViewModelProtocol {
     
     private(set) var users: [UserStatistics] = []
     
+    private var isSortByRating: Bool {
+        // TODO: нужно будет разобраться: раз в несколько запусков сохранение порядка не срабатывает(
+        
+        get {
+            let value = UserDefaults.standard.bool(forKey: Constants.UserDefaultKeys.isSortByRating)
+            return value
+        }
+        
+        set(newValue) {
+            UserDefaults.standard.setValue(newValue,
+                                           forKey: Constants.UserDefaultKeys.isSortByRating)
+        }
+    }
+    
     init() {
         loadMockData()
     }
     
     func loadMockData() {
         users = UserMock.mockStatisticsUserData
-        reloadTableView?()
+        
+        if isSortByRating {
+            sortByRating()
+        } else {
+            sortByName()
+        }
     }
     
     func sortByName() {
+        isSortByRating = false
         users.sort { $0.name < $1.name }
         reloadTableView?()
     }
     
     func sortByRating() {
+        isSortByRating = true
         users.sort { $0.score > $1.score }
         reloadTableView?()
     }
     
     func showSortOptions() {
         showSortActionSheet?()
+    }
+}
+
+private enum Constants {
+    enum UserDefaultKeys {
+        static let isSortByRating = "isSortByRating"
     }
 }
