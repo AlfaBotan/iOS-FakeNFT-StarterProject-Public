f315cd694419058836a44e745639cf9735bb3a48
Дмитрий Казаков <dmitryifile@yandex.ru>
2024-09-13 17:52:03 +0300

    Добавил загрузку NFT коллекции из сети

--- FakeNFT/Scenes /Statistics/UsersCollection/UserCollectionViewModel.swift
+++ FakeNFT/Scenes /Statistics/UsersCollection/UserCollectionViewModel.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import Kingfisher
 
 protocol UserCollectionViewModelProtocol {
+    var nftService: NftService { get }
     var numberOfItems: Int { get }
     var onDataChanged: (() -> Void)? { get set }
     
@@ -16,10 +18,17 @@ protocol UserCollectionViewModelProtocol {
 }
 
 final class UserCollectionViewModel: UserCollectionViewModelProtocol {
+    var nftService: NftService
+    
     var numberOfItems: Int {
         return nfts.count
     }
     var onDataChanged: (() -> Void)?
+    
+    init(nftService: NftService = NftServiceImpl(networkClient: DefaultNetworkClient(),
+                                                 storage: NftStorageImpl())) {
+        self.nftService = nftService
+    }
     
     private var nfts: [NFTCellModel] = [] {
         didSet {
@@ -34,12 +43,31 @@ final class UserCollectionViewModel: UserCollectionViewModelProtocol {
     
     func loadNFTs() {
         // TODO: Заменить на актуальные данные после создания сервиса
-        let mockNFTs = (1...15).map { _ in
-            NFTCellModel(image: UIImage(named:"mockNFT") ?? UIImage(),
-                         rating: 3,
-                         name: "Archie",
-                         cost: 1.78)
+//        let mockNFTs = (1...15).map { _ in
+//            NFTCellModel(image: UIImage(named:"mockNFT") ?? UIImage(),
+//                         rating: 3,
+//                         name: "Archie",
+//                         cost: 1.78)
+//        }
+        nftService.loadNfts(page: 1, size: 10) { result in
+            switch result {
+            case .success(let nfts):
+                let mockNFTS = nfts.map { nft in
+                    NFTCellModel(image: UIImage(named: "mockNFT") ?? UIImage(), 
+                                 rating: nft.rating,
+                                 name: nft.name,
+                                 cost: nft.price)
+                }
+                self.nfts = mockNFTS
+            case .failure(let error):
+                print("Localized Description: \(error.localizedDescription)")
+                if let error = error as? NSError {
+                    print("Domain: \(error.domain)")
+                    print("Code: \(error.code)")
+                    print("User Info: \(error.userInfo)")
+                }
+            }
         }
-        nfts = mockNFTs
+        
     }
 }
---
+++ FakeNFT/Services/Requests/NftsRequest.swift
@@ -0,0 +1,8 @@
+//
+//  NftsRequest.swift
+//  FakeNFT
+//
+//  Created by Дмитрий on 13.09.2024.
+//
+
+import Foundation
