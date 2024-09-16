7d23d60d2ccbafdf02b9633d3253bb808100831c
AlfaBotan <ilya.voloshik@gmail.com>
2024-09-16 13:55:02 +0300

    добавил структуру для парсинга данных из JSON

--- FakeNFT/Scenes /Catalog/CatalogScreen/ViewModel/NFTRowModel.swift
+++ FakeNFT/Scenes /Catalog/CatalogScreen/ViewModel/NFTRowModel.swift
@@ -12,3 +12,13 @@ struct NFTRowModel {
     let name: String
     let count: Int
 }
+
+struct NFTModelCatalog: Codable {
+    let createdAt: String
+    let name: String
+    let cover: String
+    let nfts: [String]
+    let description: String
+    let author: String
+    let id: String
+}
