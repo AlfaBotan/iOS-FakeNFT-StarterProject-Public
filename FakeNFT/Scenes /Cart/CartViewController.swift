5c9e036343a9a52f2ecf31a8a9706e7e2f4dde4a
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-09-23 21:19:42 +0300

    cart: Fix small bugs

--- FakeNFT/Scenes /Cart/CartViewController.swift
+++ FakeNFT/Scenes /Cart/CartViewController.swift
@@ -6,24 +6,24 @@ import UIKit
 import ProgressHUD
 
 final class CartViewController: UIViewController {
-
+  
   // MARK: - ServicesAssembly
-
+  
   init(servicesAssembly: ServicesAssembly) {
     self.servicesAssembly = servicesAssembly
     super.init(nibName: nil, bundle: nil)
   }
-
+  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
   // MARK: -  Varibles
-
+  
   let servicesAssembly: ServicesAssembly
   private let viewModel = CartViewModel()
-
-
+  
+  
   private lazy var tableView: UITableView = {
     let tableView = UITableView()
     tableView.rowHeight = 140
@@ -34,7 +34,7 @@ final class CartViewController: UIViewController {
     tableView.register(CartTableViewCell.self, forCellReuseIdentifier: "CartCell")
     return tableView
   }()
-
+  
   private lazy var placeholderLabel: UILabel = {
     let label = UILabel()
     label.font = UIFont.bodyBold
@@ -43,7 +43,7 @@ final class CartViewController: UIViewController {
     label.isHidden = true
     return label
   }()
-
+  
   private lazy var sortButton: UIButton = {
     let sortButton = UIButton()
     let image = Images.Common.sortBtn?.withTintColor(UIColor.segmentActive, renderingMode: .alwaysOriginal)
@@ -51,7 +51,7 @@ final class CartViewController: UIViewController {
     sortButton.addTarget(nil, action: #selector(sortButtonTapped), for: .touchUpInside)
     return sortButton
   }()
-
+  
   private lazy var payButton: UIButton = {
     let payButton = UIButton()
     payButton.setTitle(Strings.Cart.toPay, for: .normal)
@@ -64,7 +64,7 @@ final class CartViewController: UIViewController {
     payButton.translatesAutoresizingMaskIntoConstraints = false
     return payButton
   }()
-
+  
   private lazy var quantityNFTLabel: UILabel = {
     let label = UILabel()
     label.font = UIFont.caption1
@@ -73,7 +73,7 @@ final class CartViewController: UIViewController {
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
-
+  
   private lazy var totalAmountLabel: UILabel = {
     let label = UILabel()
     label.font = UIFont.bodyBold
@@ -82,25 +82,25 @@ final class CartViewController: UIViewController {
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
-
+  
   private lazy var backgroundView: UIView = {
     let backgroundView = UIView()
     backgroundView.backgroundColor = UIColor.segmentInactive
     backgroundView.layer.masksToBounds = true
     backgroundView.layer.cornerRadius = 12
     backgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-
+    
     backgroundView.addSubview(totalAmountLabel)
     backgroundView.addSubview(quantityNFTLabel)
     backgroundView.addSubview(payButton)
-
+    
     NSLayoutConstraint.activate([
       quantityNFTLabel.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
       quantityNFTLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 16),
-
+      
       totalAmountLabel.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
       totalAmountLabel.topAnchor.constraint(equalTo: quantityNFTLabel.bottomAnchor, constant: 8),
-
+      
       payButton.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -16),
       payButton.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 16),
       payButton.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -16),
@@ -109,13 +109,91 @@ final class CartViewController: UIViewController {
     ])
     return backgroundView
   }()
-
+  
   // MARK: - LifeCycle
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     setupAppearance()
+    loadData()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    loadData()
+    super.viewWillAppear(animated)
+    tabBarController?.tabBar.isHidden = false
+    navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: animated)
+  }
+  
+  // MARK: - Button Go To Payment Screen
+  
+  @objc private func paymentButtonTapped() {
+    let paymentViewController = PaymentViewController()
+    navigationController?.pushViewController(paymentViewController, animated: true)
+    navigationController?.navigationBar.tintColor = UIColor.segmentActive
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+  }
+  
+  // MARK: - Alert Action Sheet
+  
+  @objc private func sortButtonTapped() {
+    let actionSheet = UIAlertController(title: Strings.Alerts.sortTitle, message: nil, preferredStyle: .actionSheet)
+    
+    let priceSort = UIAlertAction(title: Strings.Alerts.sortByPrice, style: .default) { _ in
+      self.viewModel.sortByPrice()
+      self.tableView.reloadData()
+    }
+    
+    let ratingSort = UIAlertAction(title: Strings.Alerts.sortByRating, style: .default) { _ in
+      self.viewModel.sortByRating()
+      self.tableView.reloadData()
+    }
+    
+    let titleSort = UIAlertAction(title: Strings.Alerts.sortByTitle, style: .default) { _ in
+      self.viewModel.sortByName()
+      self.tableView.reloadData()
+    }
+    
+    let cancelAction = UIAlertAction(title: Strings.Alerts.closeBtn, style: .cancel)
+    
+    [priceSort, ratingSort, titleSort, cancelAction].forEach { actionSheet.addAction($0) }
+    present(actionSheet, animated: true, completion: nil)
+  }
+  
+  
+  func setupAppearance() {
+    [tableView, sortButton, placeholderLabel, backgroundView].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview($0) }
+    
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: sortButton.bottomAnchor, constant: 20),
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      tableView.bottomAnchor.constraint(equalTo: backgroundView.topAnchor),
+      
+      sortButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+      sortButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
+      sortButton.widthAnchor.constraint(equalToConstant: 42),
+      sortButton.heightAnchor.constraint(equalToConstant: 42),
+      
+      placeholderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      placeholderLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      
+      backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      backgroundView.heightAnchor.constraint(equalToConstant: 76)
+    ])
+  }
+  
+  private func loadData() {
     viewModel.onItemsUpdated = {
       DispatchQueue.main.async {
         self.tableView.reloadData()
@@ -126,90 +204,15 @@ final class CartViewController: UIViewController {
     viewModel.loadItems()
     viewModel.applySorting()
   }
-
-  override func viewWillAppear(_ animated: Bool) {
-    ProgressHUD.show()
-    ProgressHUD.animationType = .circleSpinFade
-    super.viewWillAppear(animated)
-    tabBarController?.tabBar.isHidden = false
-    navigationController?.setNavigationBarHidden(true, animated: animated)
-  }
-
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: animated)
-  }
-
-  // MARK: - Button Go To Payment Screen
-
-  @objc private func paymentButtonTapped() {
-    let paymentViewController = PaymentViewController()
-    navigationController?.pushViewController(paymentViewController, animated: true)
-    navigationController?.navigationBar.tintColor = UIColor.segmentActive
-    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-  }
-
-  // MARK: - Alert Action Sheet
-
-  @objc private func sortButtonTapped() {
-    let actionSheet = UIAlertController(title: Strings.Alerts.sortTitle, message: nil, preferredStyle: .actionSheet)
-
-    let priceSort = UIAlertAction(title: Strings.Alerts.sortByPrice, style: .default) { _ in
-      self.viewModel.sortByPrice()
-      self.tableView.reloadData()
-    }
-
-    let ratingSort = UIAlertAction(title: Strings.Alerts.sortByRating, style: .default) { _ in
-      self.viewModel.sortByRating()
-      self.tableView.reloadData()
-    }
-
-    let titleSort = UIAlertAction(title: Strings.Alerts.sortByTitle, style: .default) { _ in
-      self.viewModel.sortByName()
-      self.tableView.reloadData()
-    }
-
-    let cancelAction = UIAlertAction(title: Strings.Alerts.closeBtn, style: .cancel)
-
-    [priceSort, ratingSort, titleSort, cancelAction].forEach { actionSheet.addAction($0) }
-    present(actionSheet, animated: true, completion: nil)
-  }
-
-
-  func setupAppearance() {
-    [tableView, sortButton, placeholderLabel, backgroundView].forEach {
-      $0.translatesAutoresizingMaskIntoConstraints = false
-      view.addSubview($0) }
-
-    NSLayoutConstraint.activate([
-      tableView.topAnchor.constraint(equalTo: sortButton.bottomAnchor, constant: 20),
-      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      tableView.bottomAnchor.constraint(equalTo: backgroundView.topAnchor),
-
-      sortButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-      sortButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -9),
-      sortButton.widthAnchor.constraint(equalToConstant: 42),
-      sortButton.heightAnchor.constraint(equalToConstant: 42),
-
-      placeholderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      placeholderLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-
-      backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-      backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      backgroundView.heightAnchor.constraint(equalToConstant: 76)
-    ])
-  }
-
+  
   private func updateSummary() {
     let totalAmount = viewModel.totalAmount()
     let totalQuantity = viewModel.totalQuantity()
-
+    
     quantityNFTLabel.text = "\(totalQuantity) NFT"
-    totalAmountLabel.text = "\(totalAmount) \(Strings.Common.eth)"
+    totalAmountLabel.text = String(format: "%.2f", totalAmount) + " " + Strings.Common.eth
   }
-
+  
   private func updateHolders() {
     tableView.isHidden = viewModel.nftItems.isEmpty
     payButton.isHidden = viewModel.nftItems.isEmpty
@@ -225,7 +228,7 @@ extension CartViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return viewModel.nftItems.count
   }
-
+  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: "CartCell", for: indexPath) as? CartTableViewCell else {
       return UITableViewCell()
